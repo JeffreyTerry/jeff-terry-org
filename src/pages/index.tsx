97ffbb9b8b2b4a3d3c4fdb1bd95e92404f7c360f
Jeffrey Terry <jterry94@gmail.com>
2021-02-22 15:23:29 -0600

    added page title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import Home from '../components/Home';
-import './index.css';
 import { ThemeProvider } from 'styled-components';
+import { Helmet } from 'react-helmet';
+import './index.css';
 
 const theme = {
   bgColor: '#fff',
@@ -18,7 +19,9 @@ const theme = {
 function IndexPage() {
   return (
     <main>
-      <title>Jeff Terry</title>
+      <Helmet>
+        <title>Jeff Terry</title>
+      </Helmet>
       <ThemeProvider theme={theme}>
         <Home />
       </ThemeProvider>
