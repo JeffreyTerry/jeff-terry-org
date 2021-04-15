4f5dea43e6249296f1138f615ad0b307f3a039e8
Jeffrey Terry <jterry94@gmail.com>
2021-04-15 09:38:45 -0700

    made the deploy script way better

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
+set -e
+
 npm test
-TEST_STATUS=$?
-
-if [[ $TEST_STATUS -eq 0 ]]; then
-  npm run deploy-after-testing
-  DEPLOY_STATUS=$?
-fi
-
-exit $TEST_STATUS || $DEPLOY_STATUS
+npm run deploy-after-testing
